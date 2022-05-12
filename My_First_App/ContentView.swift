37a4718154397351185d0792c6565173cbe8630d
NicolasBarbosa0 <nicolasbarbosa0@gmail.com>
2022-05-12 09:35:59 -0300

    Initial Commit

---
+++ My_First_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My_First_App
+//
+//  Created by Nicolas Barbosa on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My_First_App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My_First_App
+//
+//  Created by Nicolas Barbosa on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
