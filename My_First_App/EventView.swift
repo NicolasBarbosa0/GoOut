9f9ba18638a86cb35bcd231bd17f1107957e731a
NicolasBarbosa0 <nicolasbarbosa0@gmail.com>
2022-05-18 10:56:52 -0300

    first commit

---
+++ My_First_App/EventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  My_First_App
+//
+//  Created by Nicolas Barbosa on 17/05/22.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventView()
+    }
+}
---
+++ My_First_App/NavBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NavBarView.swift
+//  My_First_App
+//
+//  Created by Nicolas Barbosa on 16/05/22.
+//
+
+import SwiftUI
+
+struct NavBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavBarView()
+    }
+}
